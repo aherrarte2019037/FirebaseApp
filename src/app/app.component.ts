309b73b9266066c714a0e63a7307f7b30bb87eb8
Angel Herrarte <aherrarte-2019037@kinal.edu.gt>
2021-06-05 13:34:14 -0600

    Se agregó DaisyUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  clockHour: number = 1;
-  clockMin: number = 1;
-  clockSec: number = 1;
-
-  ngOnInit(): void {
-    setInterval( () => this.clockSec ++, 1000);
-  }
 
 }
