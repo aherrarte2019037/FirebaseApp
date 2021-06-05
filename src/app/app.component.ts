f3d533c6d857b9728c8880f0619113e8adb6c3e8
Angel Herrarte <aherrarte-2019037@kinal.edu.gt>
2021-06-05 13:03:58 -0600

    Se agregaron dependencias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirebaseApp';
+  clockHour: number = 1;
+  clockMin: number = 1;
+  clockSec: number = 1;
+
+  ngOnInit(): void {
+    setInterval( () => this.clockSec ++, 1000);
+  }
+
 }
